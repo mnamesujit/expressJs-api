d15ea51313a5b72ea1df77626e8286a9c0e4fdf7
Sujit <mnamesujit@gmail.com>
2023-05-25 19:48:18 +0530

    serving static files in public directory

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const path = require( 'path')
 const app = express()
-const port = 3000
+const port = 4000
 
+// Getting path of static files directory
+const staticPath = path.join(__dirname, "./public")
+console.log(staticPath)
+
+// Serving static files in Express using express.static middleware
+app.use(express.static(staticPath))
+
+
+// default GET request for '/' path
 app.get('/', (req, res) => {
-    res.send({status: 200, message: 'everything is fine'})
+    res.send({status: 200, message: 'everything is OK'})
 })
-
 
 
 app.listen(port, ()=>{
