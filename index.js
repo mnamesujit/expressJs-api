a2c714168ffbc36f08a82dece575dedf3246a3c9
Sujit <mnamesujit@gmail.com>
2023-05-26 15:24:48 +0530

    added ejs and handling bad requests through 404.ejs error page

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const { render } = require('ejs')
 const express = require('express')
 const path = require( 'path')
 const app = express()
 const port = 4000
+
+// setting ejs path
+app.set("view engine", "ejs")
 
 // Getting path of static files directory
 const staticPath = path.join(__dirname, "./public")
@@ -16,6 +20,10 @@ app.get('/', (req, res) => {
     res.send({status: 200, message: 'everything is OK'})
 })
 
+// For handling bad requests if path does'nt exists
+app.get('*', (req, res) =>{
+    res.render('404')
+})
 
 app.listen(port, ()=>{
     console.log(`server is runnig at ${port}...`)
