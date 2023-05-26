a476d4225633b1dd4aa1480f87deeeb498f0d7f0
Sujit <mnamesujit@gmail.com>
2023-05-26 16:22:55 +0530

    make style of 404.ejs error page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { render } = require('ejs')
 const express = require('express')
 const path = require( 'path')
 const app = express()
-const port = 4000
+const port = 3000
 
 // setting ejs path
 app.set("view engine", "ejs")
